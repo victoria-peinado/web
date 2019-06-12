2b93e6e386015d919ebc1da95f07c4ad5b27b3f7
victoria-peinado <49839433+victoria-peinado@users.noreply.github.com>
2019-06-12 20:06:20 -0300

    adaptado ajax a milugar

---
+++ milugar/biblioteca/crear_tablas.php
@@ -0,0 +1,20 @@
+<?php
+include "variables.inc";
+$mysqli = new mysqli($host, $user, $pass, $base);
+
+//$mysqli->query("drop table if exists clientes");
+$cliente = $mysqli->query("create table if not exists clientes 
+	(
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		nombre VARCHAR(255) NOT NULL,
+		estado VARCHAR(20) NOT NULL,
+		tipo VARCHAR(20) NOT NULL,
+		key(nombre)
+	)");
+if ($cliente == 1) 
+	echo 'La tabla de <font color="red" size="+2">clientes</font> se cre&oacute; con &eacute;xito <br>';
+else
+	echo 'problemas con la tabla de clientes'.'<br>';
+
+?>
+
