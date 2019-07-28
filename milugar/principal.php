ef7fb8047dd1453aad0fbb8979eca0175c7adff5
victoria-peinado <49839433+victoria-peinado@users.noreply.github.com>
2019-07-28 18:18:15 -0300

    argegadologin
    hay un problema con los tildes en la pagina principal

---
+++ milugar/principal.php
@@ -0,0 +1,59 @@
+
+<html>
+
+<head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="menu.css">
+ <link rel="stylesheet" href="principal-estilo.css">
+
+</head>
+<body>
+<div class="flex-container">
+ <div><seleccionado  href="#">Mi Lugar</seleccionado ></div>
+  <div><a href="biblioteca/biblioteca.html">Biblioteca</a></div>
+  <div><a href="#">Mis cosas</a></div>
+  <div><a href="#">Mi Perfil</a></div>
+</div>
+
+<section>
+
+  
+  <article>
+    <h1>Sobre nosotros</h1>
+
+    <p>Somos un grupo pequeño que creó esta página para personas como nosotros: gente que sigue varias series, mangas, u otras cosas que se actualizan en períodos regulares.</p>
+	<p>Es difícil recordar las fechas en las que cada historia que estás leyendo se actualizan. Con nuestro servicio, no es necesario. Relájate sabiendo que no te perderás un solo episodio de esa serie con la que te obsesionaste tanto las últimas semanas.  Con nosotros, siempre vas a estar al día. </p>
+
+  </article>
+   <inicioc>
+  <form action="principal_g.php" method="post">
+	  <div>
+		  <h4>Iniciar seción</h4>
+		  <p>E-mail:</p>
+		  <input type="email" name="mail" required>
+		  <br><br>
+		  <p>Contrseña:</p>
+		  <input type="password" name="password" required>
+		  <br>
+		  <input type="submit" value="Ingresar" >
+		  <a href="registro/formulario.html">¿No tienes cuenta?</a>
+		  
+	  </div>
+	</form> 
+ </inicioc>
+</section>
+
+<footer>
+</footer>
+<section>
+  <article>
+    <h1>En emisión</h1>
+    <p>Inicie seción para acceder a esta función.</p>
+
+  </article>
+</section>
+</body>
+</html>
+
+
