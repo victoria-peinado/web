9769b3ef2037fcd4ee4cbd0265bce7dade95c0a4
victoria-peinado <49839433+victoria-peinado@users.noreply.github.com>
2019-07-28 16:32:39 -0300

    guardadounborrado

---
+++ ejemplos/guardarnosesisuardarlo.php
@@ -0,0 +1,54 @@
+<?php
+
+include "../variables.inc";
+
+$mysqli = new mysqli($host,$user,$pass);
+
+if ($mysqli->connect_error) {
+    die('Connection error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
+
+
+$charsetName = "UTF8";
+$collationName = "utf8_bin";
+
+if( !$mysqli->query("CREATE DATABASE IF NOT EXISTS " . $base . " CHARACTER SET " . $charsetName . " COLLATE " . $collationName )) {
+    die($mysqli->error);
+
+}
+$mysqli->close();
+
+$mysqli = new mysqli ($host,$user,$pass,$base);
+
+if ($mysqli->connect_error) {
+    die('Connection error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
+
+if ( !$mysqli->query("CREATE TABLE IF NOT EXISTS
+                usuarios (id INT AUTO_INCREMENT PRIMARY KEY,
+                nombre VARCHAR(255) NOT NULL,
+				mail VARCHAR(255) NOT NULL,
+				password VARCHAR(255) NOT NULL,
+				descripcion TEXT)") )  {
+                 die($mysqli->error); }
+				 
+				 
+				 
+
+$nombre = $_POST["nombre"];
+$mail = $_POST["mail"];
+$password = $_POST["clave1"];
+$text = $_POST["text"];
+$consulta = " INSERT INTO usuarios (nombre,mail,password,descripcion) VALUES  ( '" . $nombre . "' , '" . $mail . "' , '" . $password . "' , '" . $text . "' ) "  ;
+ 
+ echo $consulta;
+
+ 
+if ( !$mysqli->query( $consulta ) ) {
+	die ( $mysqli -> error );
+	//  echo $consulta;
+	}
+
+$mysqli -> close ();
+header('Location: formulario_correcto.html');
+?>
