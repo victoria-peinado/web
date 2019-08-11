8ebbe259ed2c3e31126756ce312f0f33ce256810
victoria-peinado <49839433+victoria-peinado@users.noreply.github.com>
2019-08-10 21:16:44 -0300

    agregada tabla usuario

--- milugar/biblioteca/crear_tabla_elementos.php
+++ milugar/biblioteca/crear_tabla_elementos.php
@@ -2,8 +2,8 @@
 include "../variables.inc";
 $mysqli = new mysqli($host, $user, $pass, $base);
 
-//$mysqli->query("drop table if exists clientes");
-$cliente = $mysqli->query("create table if not exists clientes 
+//$mysqli->query("drop table if exists elementos");
+$elemento = $mysqli->query("create table if not exists elementos 
 	(
 		id INT AUTO_INCREMENT PRIMARY KEY,
 		nombre VARCHAR(255) NOT NULL,
@@ -11,10 +11,10 @@ $cliente = $mysqli->query("create table if not exists clientes
 		tipo VARCHAR(20) NOT NULL,
 		key(nombre)
 	)");
-if ($cliente == 1) 
-	echo 'La tabla de <font color="red" size="+2">clientes</font> se cre&oacute; con &eacute;xito <br>';
+if ($elemento == 1) 
+	echo 'La tabla de <font color="red" size="+2">elementos</font> se cre&oacute; con &eacute;xito <br>';
 else
-	echo 'problemas con la tabla de clientes'.'<br>';
+	echo 'problemas con la tabla de elementos'.'<br>';
 
 ?>
 
--- milugar/registro/crear_tabla_usuarios.php
+++ milugar/registro/crear_tabla_usuarios.php
@@ -1,7 +1,23 @@
 <?php
-
 include "../variables.inc";
 $mysqli = new mysqli($host, $user, $pass, $base);
+
+//$mysqli->query("drop table if exists elementos");
+$usuario = $mysqli->query("create table if not exists usuarios
+	(
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		nombre VARCHAR(255) NOT NULL,
+		mail VARCHAR(255) NOT NULL,
+		password VARCHAR(255) NOT NULL,
+		descripcion TEXT,
+		key(nombre)
+	)");
+if ($usuario == 1) 
+	echo 'La tabla de <font color="red" size="+2">usuarios</font> se cre&oacute; con &eacute;xito <br>';
+else
+	echo 'problemas con la tabla de usuarios'.'<br>';
+/*
+
 
 
 if ($mysqli->connect_error) {
@@ -32,5 +48,5 @@ if ( !$mysqli->query("CREATE TABLE IF NOT EXISTS
                  die($mysqli->error);
 				 echo 'problemas con la tabla de clientes'.'<br>';}
 
-
+*/
 ?>
