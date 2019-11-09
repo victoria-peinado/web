28c4395f1768ede6ff908e56b4d04185901a9cec
victoria-peinado <victoria-peinado@u.com>
2019-11-09 19:21:13 -0300

    solo te mustra tus listas

--- milugar/mis_cosas/traer.php
+++ milugar/mis_cosas/traer.php
@@ -1,9 +1,12 @@
 <?php
 include "../variables.inc";
+
+session_start();
+
 $mysqli = new mysqli($host, $user, $pass, $base);
 header("Content-Type: application/json; charset=UTF-8");
 
-$consulta =  "select * from listas";
+$consulta =  "select * from listas where usuario_id='" . $_SESSION['logged_id'] . "' ";
 
 
 
@@ -15,7 +18,6 @@ $outp= array();
 
 for ($i=0; $i<$resu->num_rows; $i++) 
 	$outp[$i] = $resu->fetch_array(MYSQLI_ASSOC);
-
 
 
 
